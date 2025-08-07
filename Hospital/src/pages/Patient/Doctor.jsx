44c2b011456ffa7b0b4c1c09b8e68468aaf83b2f
eshancool123 <eshansenadhi5@gmail.com>
2025-08-07 23:59:19 +0530

    Doctorpage

--- Hospital/src/pages/Patient/Doctor.jsx
+++ Hospital/src/pages/Patient/Doctor.jsx
@@ -20,6 +20,7 @@ const Doctor = () => {
         
         <SearchFilter />
 
+
         {/* Doctors Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {doctorsData.map((doctor, index) => (
