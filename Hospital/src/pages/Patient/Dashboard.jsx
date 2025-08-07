8ab28ce0bb0174587352a19291c67a8f24382f8b
eshancool123 <eshansenadhi5@gmail.com>
2025-08-07 22:39:31 +0530

    CompletedAppointmentsSection for dashbord

--- Hospital/src/pages/Patient/Dashboard.jsx
+++ Hospital/src/pages/Patient/Dashboard.jsx
@@ -1,8 +1,9 @@
+// Dashboard.jsx
+
 import React from 'react';
 import AppointmentsSection from '../../components/Patient/dashboardPage/AppointmentsSection';
 import PrescriptionsSection from '../../components/Patient/dashboardPage/PrescriptionsSection';
-
-
+import CompletedAppointmentsSection from '../../components/Patient/dashboardPage/CompletedAppointmentsSection'; // Import the new component
 
 const Dashboard = () => {
   return (
@@ -10,8 +11,12 @@ const Dashboard = () => {
       <h1 className="text-3xl font-bold text-gray-800 mb-6">Hello, Eshan Senadhi</h1>
       
       <AppointmentsSection />
+
+      <CompletedAppointmentsSection />
       
       <PrescriptionsSection />
+
+      
     </div>
   );
 };
